59380aea3ea0bfe6382c40b3f33637c393400a6f
Jim Strother <strotherwebdev@gmail.com>
2016-04-10 01:39:43 +0000

    trying to make a DELETE endpoint, not entirely successful yet

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 var Storage = function() {
     this.items = [];
@@ -12,6 +14,12 @@ Storage.prototype.add = function(name) {
     return item;
 };
 
+Storage.prototype.delete = function(name) {
+    var item = {name: name, id: this.id};
+    this.items.delete(item);
+    this.id -= 1;
+};
+
 var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
@@ -22,6 +30,24 @@ app.use(express.static('public'));
 
 app.get('/items', function(req, res) {
     res.json(storage.items);
+});
+
+app.post('/items', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+    var item = storage.add(req.body.name);
+    res.status(201).json(item);
+});
+
+app.delete('/items/:id', function(req, res) {
+    if (!':id') {
+        return res.sendStatus(400);
+    } else {
+        storage.items.splice(req.params.id, 1);
+        res.json(true);
+        storage.id -= 1; //need a way to subtract 1 from all id's past the item being deleted, otherwise those "after" items won't delete
+    }
 });
 
 app.listen(process.env.PORT || 8080);
