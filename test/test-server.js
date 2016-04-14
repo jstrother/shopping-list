73f4f518696c0d6be8fcd293c4171f9dfb0c2ef2
Jim Strother <strotherwebdev@gmail.com>
2016-04-14 05:21:13 +0000

    added mocha testing

---
+++ test/test-server.js
@@ -0,0 +1,8 @@
+describe('Shopping List', function() {
+    it('should list items on get');
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should return an error if there is nothing to edit');
+    it('should delete an item on delete');
+    it('should return an error if there is nothing to remove');
+});
