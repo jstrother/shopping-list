76dc6930826f5869bcf41da81c1eade94c4c3b25
Jim Strother <jim.strother@gmail.com>
2016-05-04 16:21:00 -0700

    mongodb working; adding mongoose

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -3,53 +3,80 @@ var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost/snippets', function(err, db) {
     if (err) {
         console.error(err);
-        /*the following line commented out as it causes a TypeError "cannot
-        'close' property of undefined"*/
-        // db.close();
+        db.close();
         return;
     }
 
     var collection = db.collection('snippets');
 
     var create = function(name, content) {
-        var snippet = {
-            name: name,
-            content: content
-        };
-        collection.insert(snippet, function(err, result) {
-            if (err) {
-                console.error("Could not create snippet", name);
-                db.close();
-                return;
-            }
-            console.log("Created snippet", name);
-            db.close();
-        });
-    };
+	    var snippet = {
+	        name: name,
+	        content: content
+	    };
+	    collection.insert(snippet, function(err, result) {
+	        if (err) {
+	            console.error("Could not create snippet", name);
+	            db.close();
+	            return;
+	        }
+	        console.log("Created snippet", name);
+	        db.close();
+	    });
+	};
 
     var read = function(name) {
-        var query = {
-            name: name
-        };
-        collection.findOne(query, function(err, snippet) {
-            if (!snippet || err) {
-                console.error("Could not read snippet", name);
-                db.close();
-                return;
-            }
-            console.log("Read snippet", snippet.name);
-            console.log(snippet.content);
-            db.close();
-        });
-    };
+	    var query = {
+	        name: name
+	    };
+	    collection.findOne(query, function(err, snippet) {
+	        if (!snippet || err) {
+	            console.error("Could not read snippet", name);
+	            db.close();
+	            return;
+	        }
+	        console.log("Read snippet", snippet.name);
+	        console.log(snippet.content);
+	        db.close();
+	    });
+	};
 
     var update = function(name, content) {
-        db.close();
-    };
+	    var query = {
+	        name: name
+	    };
+
+	    var update = {
+	        $set: {content: content}
+	    };
+
+	    collection.findAndModify(query, null, update, function(err, result) {
+	        var snippet = result.value;
+	        if (!snippet || err) {
+	            console.error("Could not update snippet", name);
+	            db.close();
+	            return;
+	        }
+	        console.log("Updated snippet", snippet.name);
+	        db.close();
+	    });
+	};
 
     var del = function(name, content) {
-        db.close();
-    };
+	    var query = {
+	        name: name
+	    };
+	    collection.findAndRemove(query, function(err, result) {
+	        var snippet = result.value;
+	        if (!snippet || err) {
+	            console.error("Could not delete snippet", name);
+	            db.close();
+	            return;
+	        }
+	        console.log("Deleted snippet", snippet.name);
+	        db.close();
+	    });
+	};
 
     var main = function() {
         if (process.argv[2] == 'create') {
