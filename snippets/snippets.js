d31f28f0546922995c9de698d9fdfcdb52e4ad34
Jim Strother <jim.strother@gmail.com>
2016-05-03 17:36:04 -0700

    error in snippets/snippets.js, TypeError

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -1,12 +1,74 @@
 var MongoClient = require('mongodb').MongoClient;
 
-
-MongoClient.connect('mongodb://localhost/', function(err, db) {
+MongoClient.connect('mongodb://localhost/snippets', function(err, db) {
     if (err) {
         console.error(err);
-        db.close();
+        /*the following line commented out as it causes a TypeError "cannot
+        'close' property of undefined"*/
+        // db.close();
         return;
     }
-    console.log('Connected to MongoDB database');
-    db.close();
+
+    var collection = db.collection('snippets');
+
+    var create = function(name, content) {
+        var snippet = {
+            name: name,
+            content: content
+        };
+        collection.insert(snippet, function(err, result) {
+            if (err) {
+                console.error("Could not create snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Created snippet", name);
+            db.close();
+        });
+    };
+
+    var read = function(name) {
+        var query = {
+            name: name
+        };
+        collection.findOne(query, function(err, snippet) {
+            if (!snippet || err) {
+                console.error("Could not read snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Read snippet", snippet.name);
+            console.log(snippet.content);
+            db.close();
+        });
+    };
+
+    var update = function(name, content) {
+        db.close();
+    };
+
+    var del = function(name, content) {
+        db.close();
+    };
+
+    var main = function() {
+        if (process.argv[2] == 'create') {
+            create(process.argv[3], process.argv[4]);
+        }
+        else if (process.argv[2] == 'read') {
+            read(process.argv[3]);
+        }
+        else if (process.argv[2] == 'update') {
+            update(process.argv[3], process.argv[4]);
+        }
+        else if (process.argv[2] == 'delete') {
+            del(process.argv[3]);
+        }
+        else {
+            console.error('Command not recognized');
+            db.close();
+        }
+    };
+
+    main();
 });
