9ece8584be53f0482d1819b5b9e5621a0707f700
Jim Strother <jim.strother@gmail.com>
2016-05-05 17:10:35 -0700

    got mongo working again

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -10,73 +10,71 @@ MongoClient.connect('mongodb://localhost/snippets', function(err, db) {
     var collection = db.collection('snippets');
 
     var create = function(name, content) {
-	    var snippet = {
-	        name: name,
-	        content: content
-	    };
-	    collection.insert(snippet, function(err, result) {
-	        if (err) {
-	            console.error("Could not create snippet", name);
-	            db.close();
-	            return;
-	        }
-	        console.log("Created snippet", name);
-	        db.close();
-	    });
-	};
+    	var snippet = {
+        name: name,
+        content: content
+    	};
+    	collection.insert(snippet, function(err, result) {
+    		if (err) {
+    			console.error("Could not create snippet", name);
+    			db.close();
+    			return;
+    		}
+    		console.log("Created snippet", name)
+    		db.close();
+    	});
+    };
 
     var read = function(name) {
-	    var query = {
-	        name: name
-	    };
-	    collection.findOne(query, function(err, snippet) {
-	        if (!snippet || err) {
-	            console.error("Could not read snippet", name);
-	            db.close();
-	            return;
-	        }
-	        console.log("Read snippet", snippet.name);
-	        console.log(snippet.content);
-	        db.close();
-	    });
-	};
+    	var query = {
+    		name: name
+    	};
+    	collection.findOne(query, function(err, snippet) {
+    		if (!snippet || err) {
+    			console.error("Could not read snippet", name);
+    			db.close();
+    			return;
+    		}
+    		console.log("Read snippet", snippet.name);
+    		console.log(snippet.content);
+    		db.close();
+    	})
+    };
 
     var update = function(name, content) {
-	    var query = {
-	        name: name
-	    };
-
-	    var update = {
-	        $set: {content: content}
-	    };
-
-	    collection.findAndModify(query, null, update, function(err, result) {
-	        var snippet = result.value;
-	        if (!snippet || err) {
-	            console.error("Could not update snippet", name);
-	            db.close();
-	            return;
-	        }
-	        console.log("Updated snippet", snippet.name);
-	        db.close();
-	    });
-	};
+        var query = {
+        	name: name
+        };
+        var update = {
+        	$set: {content: content}
+        };
+        collection.findAndModify(query, null, update, function(err, result) {
+        	var snippet = result.value;
+        	if (!snippet || err) {
+        		console.error("Could not update snippet", name);
+        		db.close();
+        		return;
+        	}
+        	console.log("Updated snippet", snippet.name);
+        	db.close();
+        });
+    };
 
     var del = function(name, content) {
-	    var query = {
-	        name: name
-	    };
-	    collection.findAndRemove(query, function(err, result) {
-	        var snippet = result.value;
-	        if (!snippet || err) {
-	            console.error("Could not delete snippet", name);
-	            db.close();
-	            return;
-	        }
-	        console.log("Deleted snippet", snippet.name);
-	        db.close();
-	    });
-	};
+        var query = {
+        	name: name
+        };
+        collection.findAndRemove(query, function(err, result) {
+        	var snippet = result.value;
+        	if (!snippet || err) {
+        		console.error("Could not delete snippet", name);
+        		db.close();
+        		return;
+        	}
+        	console.log("Deleted snippet", snippet.name);
+        	db.close();
+        });
+    };
 
     var main = function() {
         if (process.argv[2] == 'create') {
